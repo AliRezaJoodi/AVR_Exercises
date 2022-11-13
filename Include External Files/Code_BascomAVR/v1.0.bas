145a27df9c7796d49c8ee144ca01c16aeed87618
Ali Reza Joodi <AliRezaJoodi@outlook.com>
2022-11-14 02:45:04 +0330

    Update

--- Include External Files/Code_BascomAVR/v1.0.bas
+++ Include External Files/Code_BascomAVR/v1.0.bas
@@ -3,23 +3,23 @@
 $regfile = "m32def.dat"
 $crystal = 1000000
 
-$include "Attachment\Config_Hardware_v1.0.txt"
+$include "Attachment\Hardware_v1.0.txt"
 
 Config Debounce = 30
 
 Do
-   Debounce Key_1 , 0 , Led_on , Sub
-   Debounce Key_2 , 0 , Led_off , Sub
+   Debounce Button1 , 0 , Led_on , Sub
+   Debounce Button2 , 0 , Led_off , Sub
 Loop
 
 End
 
 '**********************************
 Led_on:
-   Set Led
+   Led1 = Active
 Return
 
 '**********************************
 Led_off:
-   Reset Led
+   Led1 = Deactive
 Return
--- Include External Files/Code_CodeVisionAVR/v1.0.c
+++ Include External Files/Code_CodeVisionAVR/v1.0.c
@@ -3,28 +3,39 @@
 #include <mega32.h>
 #include <delay.h>
 
-#include "Attachment\Config_Hardware_v1.0.txt"
+#define OUTPUT 1
+#define INPOUT 0
+#define PULL_UP 1
+#define PULL_DOWN 0
 
-bit old_status_button1=1;
-bit old_status_button2=1;
-bit old_status_button3=1;
+#include "Attachment\Hardware_v1.0.txt"
+void Config_IO(void);
+
+bit button1_status=1;
+bit button2_status=1;
 
 void main(void){        
-    Config_IO();
-        
+    Config_IO();        
     while(1){
-        if(BUTTON1==0 && old_status_button1==1){
-            old_status_button1=0;
-            LED=1;
+        if(BUTTON1==PRESS_BUTTON && button1_status==DEFAULT_BUTTON){
+            button1_status=PRESS_BUTTON;
+            LED1=ACTIVATE_LED;
         }
-        if(BUTTON1==1){old_status_button1=1;} 
+        if(BUTTON1==DEFAULT_BUTTON){button1_status=DEFAULT_BUTTON;} 
         
-        if(BUTTON2==0 && old_status_button2==1){
-            old_status_button2=0;
-            LED=0;
+        if(BUTTON2==PRESS_BUTTON && button2_status==DEFAULT_BUTTON){
+            button2_status=PRESS_BUTTON;
+            LED1=DEACTIVATE_LED;
         }
-        if(BUTTON2==1){old_status_button2=1;}  
+        if(BUTTON2==DEFAULT_BUTTON){button2_status=DEFAULT_BUTTON;}  
     }
+}
+
+//******************************************
+void Config_IO(void){
+    BUTTON1_DDR=INPOUT; BUTTON1=PULL_UP;
+    BUTTON2_DDR=INPOUT; BUTTON2=PULL_UP;
+    LED1_DDR=OUTPUT; LED1=DEFAULT_LED;
 }
 
 
