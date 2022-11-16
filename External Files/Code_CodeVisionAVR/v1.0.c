293ce34e22b3c49489196093624e65db767234a5
Ali Reza Joodi <AliRezaJoodi@outlook.com>
2022-11-17 02:29:53 +0330

    Update

--- External Files/Code_CodeVisionAVR/v1.0.c
+++ External Files/Code_CodeVisionAVR/v1.0.c
@@ -6,35 +6,35 @@
 #include "Attachment\Hardware_v1.0.h"
 void Config_IO(void);
 
-bit button1_status=1;
-bit button2_status=1;
+bit status_button1=1;
+bit status_button2=1;
 
 void main(void){        
     Config_IO();        
     while(1){
-        if(BUTTON1==PRESS_BUTTON && button1_status==DEFAULT_BUTTON){
-            button1_status=PRESS_BUTTON;
+        if(BUTTON1==PRESS_BUTTON && status_button1==DEFAULT_BUTTON){
+            status_button1=PRESS_BUTTON;
             LED1=ACTIVATE_LED;
         }
-        if(BUTTON1==DEFAULT_BUTTON){button1_status=DEFAULT_BUTTON;} 
+        if(BUTTON1==DEFAULT_BUTTON){status_button1=DEFAULT_BUTTON;} 
         
-        if(BUTTON2==PRESS_BUTTON && button2_status==DEFAULT_BUTTON){
-            button2_status=PRESS_BUTTON;
+        if(BUTTON2==PRESS_BUTTON && status_button2==DEFAULT_BUTTON){
+            status_button2=PRESS_BUTTON;
             LED1=DEACTIVATE_LED;
         }
-        if(BUTTON2==DEFAULT_BUTTON){button2_status=DEFAULT_BUTTON;}  
+        if(BUTTON2==DEFAULT_BUTTON){status_button2=DEFAULT_BUTTON;}  
     }
 }
 
 //******************************************
-void Config_IO(void){    
-    #define OUTPUT 1
-    #define INPOUT 0
+void Config_IO(void){ 
+    #define INPUT 0   
+    #define OUTPUT !INPUT
     #define PULL_UP 1
-    #define PULL_DOWN 0
+    #define PULL_DOWN !PULL_UP
     
-    BUTTON1_DDR=INPOUT; BUTTON1_PORT=PULL_UP;
-    BUTTON2_DDR=INPOUT; BUTTON2_PORT=PULL_UP;
+    BUTTON1_DDR=INPUT; BUTTON1_PORT=PULL_UP;
+    BUTTON2_DDR=INPUT; BUTTON2_PORT=PULL_UP;
     LED1_DDR=OUTPUT; LED1_PORT=DEFAULT_LED;
 }
 
