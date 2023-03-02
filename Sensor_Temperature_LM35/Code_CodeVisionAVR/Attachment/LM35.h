0c66978e7c3aeb1ae590d14681def39aed37c3fc
Ali Reza Joodi <AliRezaJoodi@outlook.com>
2023-03-02 14:50:45 +0330

    Update

--- Sensor_Temperature_LM35/Code_CodeVisionAVR/Attachment/LM35.h
+++ Sensor_Temperature_LM35/Code_CodeVisionAVR/Attachment/LM35.h
@@ -1,16 +1,35 @@
 // GitHub Account:  GitHub.com/AliRezaJoodi
 
-#ifndef CH_LM35
-    #define CH_LM35   7
+#ifndef INCLUDED_LM35
+    #define INCLUDED_LM35
+
+#ifndef PORTS_LM35
+    #define PORTS_LM35
+    
+    #define CH_LM35         7 
+    #define GAIN_LM35       0.1
 #endif
 
-#define GAIN_LM35 1/10
+#pragma used+
 
 //******************************************
-//Input:    ADC Channel Voltage(mV)
+//Input:    Voltage(mV)
 //Output:   Temp(^C)
-float Get_Temp_LM35(float mv){
+float Convert_mVtoTemp_LM35(float mv){
     float temp=0;
     temp = mv*GAIN_LM35;
     return temp;
 }
+
+//******************************************
+//Input:    Voltage(V)
+//Output:   Temp(^C)
+float Convert_VtoTemp_LM35(float volt){
+    float temp=0;
+    temp = Convert_mVtoTemp_LM35(volt*1000);
+    return temp;
+}
+
+#pragma used-
+
+#endif
--- Sensor_Temperature_LM35/Simulate/ReadMe.md
+++ Sensor_Temperature_LM35/Simulate/ReadMe.md
@@ -1,6 +1,6 @@
 ### Description
 
-V1.0:
+v1.0:
 - Last opened with Proteus 8 Professional v8.13 SP0
 - Usable for `Code_BascomAVR`
 - Usable for `Code_CodeVisionAVR`
