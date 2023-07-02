9a0b0402f111752b294463438179f749af343978
Ali Reza Joodi <AliRezaJoodi@outlook.com>
2023-07-03 01:14:31 +0330

    Update

---
+++ RTC_DS1307/Code_CodeVisionAVR_I2C/Attachment/RTC_DS1307.h
@@ -0,0 +1,70 @@
+// GitHub Account: GitHub.com/AliRezaJoodi
+
+#ifdef _TWI_INCLUDED_
+    #include <ds1307_twi.h>     
+#else
+    #include <ds1307.h>    
+#endif
+
+#ifndef _INCLUDED_DS1307
+    #define _INCLUDED_DS1307
+    
+    //#define DISABLE_DS1307
+    #ifndef DISABLE_DS1307
+        #define ENABLE_DS1307
+    #endif
+
+    #define _RS_1HZ             0b00
+    #define _RS_4096HZ          0b01
+    #define _RS_8192HZ          0b10
+    #define _RS_32768HZ         0b11
+    #define _SQWE_ENABLE        1
+    #define _OUT_HIGH           1 
+
+    #pragma used+
+    struct ds1307{
+        unsigned char day;
+        unsigned char month;
+        unsigned char year;
+        unsigned char week;
+        unsigned char hour;
+        unsigned char minute;
+        unsigned char second;
+    };
+    struct ds1307 gregorian;
+    #pragma used-
+
+//*********************************************
+void ConfigDS1307(void){ 
+    #ifdef ENABLE_DS1307
+        rtc_init(_RS_1HZ,_SQWE_ENABLE,_OUT_HIGH); 
+    #endif
+}
+        
+#pragma used+
+
+//*********************************************
+void GetTimeAndDateFromRTC(void){
+    #ifdef ENABLE_DS1307
+        rtc_get_time(&gregorian.hour,&gregorian.minute,&gregorian.second);
+        rtc_get_date(&gregorian.week,&gregorian.day,&gregorian.month,&gregorian.year);
+    #endif
+}
+
+//*********************************************
+void GetTimeFromRTC(void){
+    #ifdef ENABLE_DS1307
+        rtc_get_time(&gregorian.hour,&gregorian.minute,&gregorian.second);
+    #endif
+}
+
+//*********************************************
+void GetDateFromRTC(void){
+    #ifdef ENABLE_DS1307
+        rtc_get_date(&gregorian.week,&gregorian.day,&gregorian.month,&gregorian.year);
+    #endif
+}
+
+#pragma used-
+
+#endif
--- RTC_DS1307/Code_CodeVisionAVR_TWI/v1.0.c
+++ RTC_DS1307/Code_CodeVisionAVR_TWI/v1.0.c
@@ -2,30 +2,22 @@
 
 #include <mega32a.h>
 #include <alcd.h>
-#include <twi.h>
-#include <ds1307_twi.h>
 #include <delay.h>
 #include <stdio.h>
 #include <stdlib.h>
 
-unsigned char day=20;
-unsigned char month=01;
-unsigned char year=23;
-unsigned char week_day=0;
-unsigned char hour=10;
-unsigned char minute=20;
-unsigned char second=30;
+#include <twi.h>
+#include "Attachment\RTC_DS1307.h"
     
 void ConfigLCD(void);
 void DisplayMainPage(void);
-void ConfigI2C(void);
-void ConfigDS1307(void);
+void ConfigTWI(void);
 
 void main(void){
     //ACSR=0x80; 
     //SFIOR=0x00; 
 
-    ConfigI2C();
+    ConfigTWI();
     ConfigDS1307();      
     ConfigLCD(); 
     
@@ -35,12 +27,18 @@ void main(void){
     //rtc_set_date(week_day,day,month,year);
     
     while(1){
-        rtc_get_time(&hour,&minute,&second);
-        rtc_get_date(&week_day,&day,&month,&year);
-         
+        GetTimeAndDateFromRTC();
         DisplayMainPage();
         delay_ms(250);
     };
+}
+
+//*********************************************
+void ConfigTWI(void){
+    // TWI initialization
+    // Mode: TWI Master
+    // Bit Rate: 100 kHz
+    twi_master_init(100);
 }
 
 //**********************************************
@@ -48,8 +46,8 @@ void DisplayMainPage(void){
     char txt1[16];  
     char txt2[16];
     
-    sprintf(txt1,"Time: %02u:%02u:%02u",hour,minute,second);
-    sprintf(txt2,"Date: %02u/%02u/%02u",day,month,year);  
+    sprintf(txt1,"Time: %02u:%02u:%02u",gregorian.hour,gregorian.minute,gregorian.second);
+    sprintf(txt2,"Date: %02u/%02u/%02u",gregorian.day,gregorian.month,gregorian.year);  
     
     lcd_clear();
     lcd_gotoxy(0,0); lcd_puts(txt1);  
@@ -57,33 +55,11 @@ void DisplayMainPage(void){
     //lcd_gotoxy(0,1); lcd_putsf("RTC with DS1307");
 }
 
-//*********************************************
-void ConfigI2C(void){
-    // TWI initialization
-    // Mode: TWI Master
-    // Bit Rate: 100 kHz
-    twi_master_init(100);
-}
-
-//*********************************************
-void ConfigDS1307(void){
-    #define _1HZ                0
-    #define _4K096HZ            1
-    #define _8K192HZ            2
-    #define _32K768HZ           3
-    #define _ENABLE             1
-
-    #define OUT_DS1307          0 
-    #define SQWE_DS1307         _ENABLE
-    #define FREQUENCY_DS1307    _1HZ
-    
-    rtc_init(FREQUENCY_DS1307,SQWE_DS1307,OUT_DS1307); 
-}
-
 //**********************************************
 void ConfigLCD(void){
     lcd_init(16); lcd_clear();   
 }
+
 
 
 
