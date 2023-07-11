498f6bf9313c5a5f6c80a2a6caec3818ffb11657
Ali Reza Joodi <AliRezaJoodi@outlook.com>
2023-07-11 19:57:51 +0330

    Update

--- Timer1_PWM/Code_CodeVisionAVR/Attachment/Converter.h
+++ Timer1_PWM/Code_CodeVisionAVR/Attachment/Converter.h
@@ -1,8 +1,10 @@
 //GitHub Account:     GitHub.com/AliRezaJoodi
 
+#pragma used+
+
 // Input(float):    0~100%
 // Output(int):    0~1023
-unsigned int Get_0to1023(float x){
+unsigned int Convert_0to100_0to1023(float x){
     unsigned int y=0;
     if(x<0){x=0;} else if(x>100){x=100;}
     x=x*1023/100; y=x;
@@ -11,9 +13,11 @@ unsigned int Get_0to1023(float x){
 
 // Input(float):    0~100%
 // Output(char):    0~255
-unsigned char Get_0to255(float x){
+unsigned char Convert_0to100_0to255(float x){
     unsigned char y=0;
     if(x<0){x=0;} else if(x>100){x=100;}
     x=x*255/100; y=x;
     return y;       
 }
+
+#pragma used+
--- Timer1_PWM/Code_CodeVisionAVR/v1.0.c
+++ Timer1_PWM/Code_CodeVisionAVR/v1.0.c
@@ -1,53 +1,49 @@
-//GitHub Account:     GitHub.com/AliRezaJoodi
+//GitHub Account: GitHub.com/AliRezaJoodi
 
 #include <mega32a.h>
 #include <delay.h>
 #include "Attachment\Converter.h"
 
-void Config_Timer1_PWM(void);
+void ConfigTimer1ForPWM(void);
 
-void main(void){
-    unsigned int value1;
-    unsigned int value2;
-    
-    Config_Timer1_PWM();
-     
-    value1=Get_0to1023(50); OCR1A=value1; //0~1023 
-    value2=Get_0to1023(75); OCR1B=value2; //0~1023 
+void main(void){  
+    ConfigTimer1ForPWM();
+    OCR1A=Convert_0to100_0to1023(50); 
+    OCR1B=Convert_0to100_0to1023(75);  
     
     while(1){          
     }
 }
 
 //**********************************
-void Config_Timer1_PWM(void){
+void ConfigTimer1ForPWM(void){
     DDRD.4=1;
     DDRD.5=1;
 
-    // Timer/Counter 1 initialization
-    // Clock source: System Clock
-    // Clock value: 125.000 kHz
-    // Mode: Ph. correct PWM top=0x03FF
-    // OC1A output: Non-Inverted PWM
-    // OC1B output: Inverted PWM
-    // Noise Canceler: Off
-    // Input Capture on Falling Edge
-    // Timer Period: 16.368 ms
-    // Output Pulse(s):
-    // OC1A Period: 16.368 ms Width: 0 us
-    // OC1B Period: 16.368 ms Width: 16.368 ms
-    // Timer1 Overflow Interrupt: Off
-    // Input Capture Interrupt: Off
-    // Compare A Match Interrupt: Off
-    // Compare B Match Interrupt: Off
-    TCCR1A=(1<<COM1A1) | (0<<COM1A0) | (1<<COM1B1) | (1<<COM1B0) | (1<<WGM11) | (1<<WGM10);
-    TCCR1B=(0<<ICNC1) | (0<<ICES1) | (0<<WGM13) | (0<<WGM12) | (0<<CS12) | (1<<CS11) | (1<<CS10);
-    TCNT1H=0x00;
-    TCNT1L=0x00;
-    ICR1H=0x00;
-    ICR1L=0x00;
-    OCR1AH=0x00;
-    OCR1AL=0x00;
-    OCR1BH=0x00;
-    OCR1BL=0x00;
+// Timer/Counter 1 initialization
+// Clock source: System Clock
+// Clock value: 1000.000 kHz
+// Mode: Fast PWM top=0x03FF
+// OC1A output: Non-Inverted PWM
+// OC1B output: Inverted PWM
+// Noise Canceler: Off
+// Input Capture on Falling Edge
+// Timer Period: 1.024 ms
+// Output Pulse(s):
+// OC1A Period: 1.024 ms Width: 0 us
+// OC1B Period: 1.024 ms Width: 1.024 ms
+// Timer1 Overflow Interrupt: Off
+// Input Capture Interrupt: Off
+// Compare A Match Interrupt: Off
+// Compare B Match Interrupt: Off
+TCCR1A=(1<<COM1A1) | (0<<COM1A0) | (1<<COM1B1) | (1<<COM1B0) | (1<<WGM11) | (1<<WGM10);
+TCCR1B=(0<<ICNC1) | (0<<ICES1) | (0<<WGM13) | (1<<WGM12) | (0<<CS12) | (1<<CS11) | (0<<CS10);
+TCNT1H=0x00;
+TCNT1L=0x00;
+ICR1H=0x00;
+ICR1L=0x00;
+OCR1AH=0x00;
+OCR1AL=0x00;
+OCR1BH=0x00;
+OCR1BL=0x00;
 }
