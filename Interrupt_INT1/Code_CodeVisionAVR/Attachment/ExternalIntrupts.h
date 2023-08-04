3087a3b37ac088444611eb766a401ac0b81b0811
Ali Reza Joodi <AliRezaJoodi@outlook.com>
2023-08-04 23:32:30 +0330

    Update

--- Interrupt_INT1/Code_CodeVisionAVR/Attachment/ExternalIntrupts.h
+++ Interrupt_INT1/Code_CodeVisionAVR/Attachment/ExternalIntrupts.h
@@ -9,25 +9,25 @@
 #endif
 
 #ifndef EnableGlobalInterrupt
-    #define EnableGlobalInterrupt(STATUS)     if(STATUS){#asm("sei")} else{#asm("cli")}
+    #define EnableGlobalInterrupt(STATUS)       if(STATUS){#asm("sei")} else{#asm("cli")}
 #endif
 
 #ifndef _INCLUDED_INTRUPTS
     #define _INCLUDED_INTRUPTS
         
     // Interrupt Sense Control
-    #define LOW_LEVEL               	0b00   //(0<<ISC01) | (0<<ISC00)
-    #define ANY_CHANGE               	0b01   //(0<<ISC01) | (1<<ISC00)
-    #define FALLING_EDGE               	0b10   //(1<<ISC01) | (0<<ISC00)
-    #define RISING_EDGE               	0b11   //(1<<ISC01) | (1<<ISC00)
+    #define LOW_LEVEL               	        0b00   //(0<<ISC01) | (0<<ISC00)
+    #define ANY_CHANGE               	        0b01   //(0<<ISC01) | (1<<ISC00)
+    #define FALLING_EDGE               	        0b10   //(1<<ISC01) | (0<<ISC00)
+    #define RISING_EDGE               	        0b11   //(1<<ISC01) | (1<<ISC00)
     
     // Commands
-    #define SET_INT0_SENS(MODE)         MCUCR=(MCUCR & 0b11111100) | MODE; 
-    #define SET_INT1_SENS(MODE)         MCUCR=(MCUCR & 0b11110011) | (MODE<<2);
-    #define SET_INT2_SENS(MODE)         if(MODE==FALLING_EDGE){CLRBIT(MCUCSR,ISC2);} else if(MODE==RISING_EDGE){SETBIT(MCUCSR,ISC2);}
-    #define ENABLE_INT0(STATUS)         if(STATUS){SETBIT(GICR,INT0); SETBIT(GIFR,INTF0);} else{CLRBIT(GICR,INT0);}
-    #define ENABLE_INT1(STATUS)         if(STATUS){SETBIT(GICR,INT1); SETBIT(GIFR,INTF1);} else{CLRBIT(GICR,INT1);}
-    #define ENABLE_INT2(STATUS)         if(STATUS){SETBIT(GICR,INT2); SETBIT(GIFR,INTF2);} else{CLRBIT(GICR,INT2);}
+    #define SetInterrupt0SenseControl(MODE)     MCUCR=(MCUCR & 0b11111100) | MODE; 
+    #define SetInterrupt1SenseControl(MODE)     MCUCR=(MCUCR & 0b11110011) | (MODE<<2);
+    #define SetInterrupt2SenseControl(MODE)     if(MODE==FALLING_EDGE){CLRBIT(MCUCSR,ISC2);} else if(MODE==RISING_EDGE){SETBIT(MCUCSR,ISC2);}
+    #define EnableInterrupt0(STATUS)            if(STATUS){SETBIT(GICR,INT0); SETBIT(GIFR,INTF0);} else{CLRBIT(GICR,INT0);}
+    #define EnableInterrupt1(STATUS)            if(STATUS){SETBIT(GICR,INT1); SETBIT(GIFR,INTF1);} else{CLRBIT(GICR,INT1);}
+    #define EnableInterrupt2(STATUS)            if(STATUS){SETBIT(GICR,INT2); SETBIT(GIFR,INTF2);} else{CLRBIT(GICR,INT2);}
         
 #pragma used+
 
@@ -53,16 +53,16 @@ interrupt [EXT_INT2] void ext_int2_isr(void){
 //**************************************
 void ConfigExternalInterrupts(void){
     DDRD.2=0; PORTD.2=1;
-    SET_INT0_SENS(RISING_EDGE);
-    ENABLE_INT0(0);
+    SetInterrupt0SenseControl(RISING_EDGE);
+    EnableInterrupt0(0);
     
     DDRD.3=0; PORTD.3=1;
-    SET_INT1_SENS(RISING_EDGE);
-    ENABLE_INT1(1);
+    SetInterrupt1SenseControl(RISING_EDGE);
+    EnableInterrupt1(1);
     
     DDRB.2=0; PORTB.2=1;
-    SET_INT2_SENS(RISING_EDGE);
-    ENABLE_INT2(0);
+    SetInterrupt2SenseControl(RISING_EDGE);
+    EnableInterrupt2(0);
 
     EnableGlobalInterrupt(1);
 }    
