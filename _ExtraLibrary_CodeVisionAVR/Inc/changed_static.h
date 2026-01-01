e1472350b9aa4ee20f327ad2def3da72ad6453a6
Ali Reza Joodi <AliRezaJoodi@outlook.com>
2026-01-02 03:20:01 +0330

    chore: initial project setup

---
+++ _ExtraLibrary_CodeVisionAVR/Inc/changed_static.h
@@ -0,0 +1,16 @@
+// GitHub Account:  GitHub.com/AliRezaJoodi
+
+#include <stdint.h>
+
+#ifndef _CHANGED_INCLUDED
+#define _CHANGED_INCLUDED
+
+#pragma used+
+uint8_t Changed_Exact_u16(uint16_t value);
+uint8_t Changed_Threshold_u16(uint16_t value, uint16_t threshold);
+uint8_t Changed_Threshold_f32(float value, float threshold);
+#pragma used-
+
+#pragma library changed_static.lib
+    
+#endif
