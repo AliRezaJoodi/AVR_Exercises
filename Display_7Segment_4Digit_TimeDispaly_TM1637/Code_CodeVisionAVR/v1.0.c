00fe97271a3617d0f1391db7ebbacad9cb4db84c
Ali Reza Joodi <AliRezaJoodi@outlook.com>
2026-01-15 14:49:44 +0330

    chore: initial project setup

---
+++ Display_7Segment_4Digit_TimeDispaly_TM1637/Code_CodeVisionAVR/v1.0.c
@@ -0,0 +1,28 @@
+// GitHub Account:  GitHub.com/AliRezaJoodi
+
+#include <mega32a.h>
+#include <delay.h>
+#include <stdint.h>
+
+#include <tm1637.h>
+#include <display7segment4digit_decoder_time.h>
+
+void main(void){
+    uint8_t segments[4];
+
+    Time_t time_t = {
+        .hour  = 23,
+        .minute  = 59,
+        .second = 50,
+        .colon  = 1
+    };
+                 
+    TM1637_Config();
+
+    Display7Segment4Digit_DecodeTime_HourMin(time_t, &segments[0]);        
+    TM1637_SetSegments(segments, 4, 0);
+    
+    while(1){
+                                
+    };
+}
---
+++ Display_7Segment_4Digit_TimeDispaly_TM1637/Simulate/ReadMe.md
@@ -0,0 +1,3 @@
+### v1.0
+- Usable for `Code_CodeVisionAVR`
+- Saved in **Proteus 8 Professional v8.13 SP0**
