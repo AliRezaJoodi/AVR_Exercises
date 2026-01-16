67d25c7d25817abb1299423029867c79080b01f1
Ali Reza Joodi <AliRezaJoodi@outlook.com>
2026-01-16 04:38:43 +0330

    chore: update

--- Display_7Segment_6Digit_TM1637/Code_CodeVisionAVR/v1.0.c
+++ Display_7Segment_6Digit_TM1637/Code_CodeVisionAVR/v1.0.c
@@ -2,32 +2,25 @@
 
 #include <mega32a.h>
 #include <delay.h>
-//#include <stdlib.h>
 #include <stdint.h>
 
 #include <tm1637.h>
-//#include <display7segment1digit_decoder.h>
-#include <display7segment4digit_decoder_value.h>
+#include <display7segment1digit_decoder.h>
 
 void main(void){
     uint8_t segments[6];
     uint8_t i=0; 
-
-                 
+                
     TM1637_Config();
-
   
     for (i=0; i<6; ++i){
         Display7Segment1Digit_DecodeDigit(i, &segments[i]);    
     }
         
-    TM1637_SetSegments(segments, 6, 0);
-    delay_ms(500);
-    TM1637_SetSegments_FixedAddress(segments[5], 2);
-    delay_ms(500);
-    TM1637_SendCommand(0b10000111);
-    delay_ms(500);
-    TM1637_SetOnOff(1);
+    TM1637_SetSegments(segments, 6, 0); delay_ms(500);
+    TM1637_SetSegments_FixedAddress(segments[5], 2); delay_ms(500);
+    TM1637_SendCommand(0b10000111); delay_ms(500); 
+    TM1637_SetDisplay(1, 7); 
     
     while(1){
                                  
--- _ExtraLibrary_CodeVisionAVR/Inc/tm1637.h
+++ _ExtraLibrary_CodeVisionAVR/Inc/tm1637.h
@@ -13,13 +13,50 @@
 void TM1637_Config(void);
 
 #pragma used+
-//void TM1637_Start(void);
-//void TM1637_Stop(void);
-//uint8_t TM1637_WriteByte(uint8_t data);
 void TM1637_SendCommand(uint8_t command);
-void TM1637_SetOnOff(uint8_t status);
-void TM1637_SetBrightness(uint8_t value_1To7);
+
+/*
+Display control:
+    onoff: 0 or 1
+        1: Display ON
+        0: Display OFF
+    brightness: 0~ 7
+        0: Pulse width is set as 1/16. 
+        1: Pulse width is set as 2/16.
+        2: Pulse width is set as 4/16.
+        3: Pulse width is set as 10/16.
+        4: Pulse width is set as 11/16.
+        5: Pulse width is set as 12/16.
+        6: Pulse width is set as 13/16.
+        7: Pulse width is set as 14/16.     
+*/
+void TM1637_SetDisplay(uint8_t onoff, uint8_t brightness);
+
+/*
+Write SRAM data in address auto increment 1 mode.
+    segments[]: 1~6 Byte for SEG pins status.
+    length: 1 to 6
+    address: 0 to 5
+        0: GRID1
+        1: GRID2
+        2: GRID3
+        3: GRID4
+        4: GRID5
+        5: GRID6
+*/
 void TM1637_SetSegments(uint8_t segments[], uint8_t length, uint8_t address);
+
+/*
+Write SRAM data in a fixed address mode
+    data: One Byte for SEG pins status.
+    address: 0 to 5
+        0: GRID1
+        1: GRID2
+        2: GRID3
+        3: GRID4
+        4: GRID5
+        5: GRID6
+*/
 void TM1637_SetSegments_FixedAddress(uint8_t data, uint8_t address);
 #pragma used-
 
