1439e3b2ff5af7de64a4b437cc472834c240fcce
Ali Reza Joodi <AliRezaJoodi@outlook.com>
2026-01-17 01:29:02 +0330

    chore: update

--- _ExtraLibrary_CodeVisionAVR/Inc/button.h
+++ _ExtraLibrary_CodeVisionAVR/Inc/button.h
@@ -13,21 +13,33 @@
 #define BUTTON_LAG3         10000       // Long Press Lag, Value: 0~65535
 
 typedef struct{
-    volatile unsigned char  *btn_ddr;
-    volatile unsigned char  *btn_pin;
-    volatile unsigned char  *btn_port;
-    uint8_t                 btn_bit;
-    uint8_t                 status;
-    uint16_t                counter;
+    volatile uint8_t    *btn_ddr;
+    volatile uint8_t    *btn_pin;
+    volatile uint8_t    *btn_port;
+    uint8_t             btn_bit;
+    uint8_t             status;
+    uint16_t            counter;
 } Button_t;
 
-void    Button_Config                   (Button_t *btn);
+/*
+Example in main code:
+Button_t button1_t = {
+    .btn_ddr  = &DDRB,
+    .btn_pin  = &PINB,
+    .btn_port = &PORTB,
+    .btn_bit  = 0,
+    .status   = 0,
+    .counter  = 0
+};
+*/
+
+void Button_Config (Button_t *btn);
 
 #pragma used+
-void    Button_DisablePullUp            (Button_t *btn);
-uint8_t Button_SingleClick              (Button_t *btn);
-uint8_t Button_AutoRepeat_NonBlocking   (Button_t *btn);
-uint8_t Button_LongPress_NonBlocking    (Button_t *btn);
+void Button_DisablePullUp (Button_t *btn);
+uint8_t Button_SingleClick (Button_t *btn);
+uint8_t Button_AutoRepeat_NonBlocking (Button_t *btn);
+uint8_t Button_LongPress_NonBlocking (Button_t *btn);
 #pragma used-
 
 #pragma library button.lib
