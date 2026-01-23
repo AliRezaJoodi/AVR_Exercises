682fa7aa8d18a91055321be699dad9910df59c1a
Ali Reza Joodi <AliRezaJoodi@outlook.com>
2026-01-24 03:21:36 +0330

    chore: initial project setup

---
+++ Display_7Segment_6Digit_TM1637_Fixed/Code_CodeVisionAVR/v1.0.c
@@ -0,0 +1,30 @@
+// GitHub Account:  GitHub.com/AliRezaJoodi
+
+#include <mega32a.h>
+#include <delay.h>
+#include <stdint.h>
+
+#include <tm1637_fixed.h>
+#include <display7segment1digit_decoder.h>
+
+void main(void){
+    uint8_t segments[6];
+    uint8_t i=0; 
+                
+    TM1637_Config();
+  
+    for (i=0; i<6; ++i){
+        Display7Segment1Digit_DecodeDigit(i, &segments[i]);    
+    }
+        
+    TM1637_SetSegments(segments, 6, 0); delay_ms(1000); 
+    TM1637_SetSegments_FixedAddress(segments[5], 2); delay_ms(1000);
+    TM1637_ResetSegments(); delay_ms(1000);
+    TM1637_SetSegments_FixedAddress(segments[2], 5); delay_ms(1000);    
+    TM1637_SendCommand(0b10000111); delay_ms(1000); //Turn off the display 
+    TM1637_SetDisplay(1, 7); 
+    
+    while(1){
+                                 
+    };
+}
---
+++ _ExtraLibrary_CodeVisionAVR/Inc/tm1637_fixed.h
@@ -0,0 +1,77 @@
+// GitHub Account: GitHub.com/AliRezaJoodi
+
+#ifndef _TM1637_FIXED_INCLUDED
+#define _TM1637_FIXED_INCLUDED
+
+
+#include <stdint.h>
+#include <io.h>
+#include <delay.h>
+
+#include <utility_bit.h>
+ 
+#define TM1637_CLK_DDR         DDRC
+#define TM1637_CLK_PIN         PINC
+#define TM1637_CLK_PORT        PORTC
+#define TM1637_CLK_BIT         0
+
+#define TM1637_DIO_DDR         DDRC
+#define TM1637_DIO_PIN         PINC
+#define TM1637_DIO_PORT        PORTC
+#define TM1637_DIO_BIT         1
+
+#define TM1637_HALF_BIT_US     1
+
+void TM1637_Config(void);
+
+#pragma used+
+void TM1637_SendCommand(uint8_t command);
+
+/*
+Display control:
+    onoff: 0 or 1
+        1: Display ON
+        0: Display OFF
+    brightness: 0~ 7
+        0: Pulse width is set as 1/16. 
+        1: Pulse width is set as 2/16.
+        2: Pulse width is set as 4/16.
+        3: Pulse width is set as 10/16.
+        4: Pulse width is set as 11/16.
+        5: Pulse width is set as 12/16.
+        6: Pulse width is set as 13/16.
+        7: Pulse width is set as 14/16.     
+*/
+uint8_t TM1637_SetDisplay(uint8_t onoff, uint8_t brightness);
+
+/*
+Clear all digits.
+reset Seg1 to Seg10.
+*/
+void TM1637_ResetSegments(void);
+
+/*
+Write SRAM data in address auto increment 1 mode.
+    segments[]: 1~6 Byte for SEG pins status.
+    length: 1 to 6
+    address: 0 to 5
+        0: GRID1
+        ...
+        5: GRID6
+*/
+uint8_t TM1637_SetSegments(uint8_t segments[], uint8_t length, uint8_t address);
+
+/*
+Write SRAM data in a fixed address mode
+    data: One Byte for SEG pins status.
+    address: 0 to 5
+        0: GRID1
+        ...
+        5: GRID6
+*/
+uint8_t TM1637_SetSegments_FixedAddress(uint8_t data, uint8_t address);
+#pragma used-
+
+
+#pragma library tm1637_fixed.lib
+#endif
