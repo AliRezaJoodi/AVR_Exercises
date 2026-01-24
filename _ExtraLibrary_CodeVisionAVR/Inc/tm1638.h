0e271aeae952d8c1353a11aeec000c49f4dea309
Ali Reza Joodi <AliRezaJoodi@outlook.com>
2026-01-25 03:19:32 +0330

    chore: update

--- _ExtraLibrary_CodeVisionAVR/Inc/tm1638.h
+++ _ExtraLibrary_CodeVisionAVR/Inc/tm1638.h
@@ -5,10 +5,25 @@
 
 
 #include <stdint.h>
+#include <io.h>
 #include <delay.h>
 
 #include <utility_bit.h> 
-#include <tm1638_hw.h> 
+
+#define TM1638_STB_DDR         DDRA
+#define TM1638_STB_PIN         PINA
+#define TM1638_STB_PORT        PORTA
+#define TM1638_STB_BIT         7
+
+#define TM1638_CLK_DDR         DDRC
+#define TM1638_CLK_PIN         PINC
+#define TM1638_CLK_PORT        PORTC
+#define TM1638_CLK_BIT         7
+
+#define TM1638_DIO_DDR         DDRC
+#define TM1638_DIO_PIN         PINC
+#define TM1638_DIO_PORT        PORTC
+#define TM1638_DIO_BIT         6 
 
 /*
 TM1637_BIT_US Unit: uS  
