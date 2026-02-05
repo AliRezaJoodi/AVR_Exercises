f7f4292f45bbd439054a1e2249331d40593e7cc0
Ali Reza Joodi <AliRezaJoodi@outlook.com>
2026-02-06 01:03:04 +0330

    chore: initial project setup

---
+++ _Legacy/ADC_External_MCP3201_SPI_Fixed/Code_CodeVisionAVR/hardware.h
@@ -0,0 +1,15 @@
+#ifndef _HARDWARE_INCLUDED
+#define _HARDWARE_INCLUDED
+
+
+#define MCP3201_HARDWARE
+    #define MCP3201_CS_DDR      DDRB
+    #define MCP3201_CS_PORT     PORTB
+    #define MCP3201_CS_PIN      PINB
+    #define MCP3201_CS_BIT      4  
+        
+    #define MCP3201_VREF        5000     //mv       
+
+
+#endif
+
