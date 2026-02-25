79dae4b126d3f5a253fabf9feab2ec8801d42d0b
Ali Reza Joodi <AliRezaJoodi@outlook.com>
2026-02-26 02:55:30 +0330

    chore: update project, Button

--- Button/Code_CodeVisionAVR/hardware.h
+++ Button/Code_CodeVisionAVR/hardware.h
@@ -2,9 +2,9 @@
 #define HARDWARE_INCLUDED
 
 #define BUTTON_CONFIG
-    #define BUTTON_SINGLE_CLICK_LAG     30U         /**< Single Click Lag, Value: 0~65535, Unit: ms */
-    #define BUTTON_AUTO_REPEAT_LAG      500U        /**< Auto Repeat Lag, Value: 0~65535 */
-    #define BUTTON_LONG_PRESS_LAG       10000U      /**< Long Press Lag, Value: 0~65535 */
+    #define BUTTON_SINGLE_CLICK_LAG     30U         /* Single Click Lag, Value: 0~65535, Unit: ms */
+    #define BUTTON_AUTO_REPEAT_LAG      500U        /* Auto Repeat Lag, Value: 0~65535 */
+    #define BUTTON_LONG_PRESS_LAG       10000U      /* Long Press Lag, Value: 0~65535 */
 
 #define BUTTON1_DDR         DDRB
 #define BUTTON1_PIN         PINB
