0d7c06bb07748ddeb4d371e384ba9cfe5177ad88
Ali Reza Joodi <AliRezaJoodi@outlook.com>
2026-03-15 10:32:25 +0330

    chore: update mcp3204

--- ADC_External_MCP3204_SPI/Code_CodeVisionAVR/main.c
+++ ADC_External_MCP3204_SPI/Code_CodeVisionAVR/main.c
@@ -21,11 +21,13 @@ void main(void){
     uint16_t in1 = 0;
     uint16_t in2 = 0;
 
-    MCP3204_t mcp1;
-
-    mcp1.cs.ddr     = &MCP3204_CS_DDR;
-    mcp1.cs.port    = &MCP3204_CS_PORT;
-    mcp1.cs.index   =  MCP3204_CS_BIT;
+    MCP3204_t mcp1 = {
+        .cs = {
+            .ddr   = &MCP3204_CS_DDR,
+            .port  = &MCP3204_CS_PORT,
+            .index = MCP3204_CS_BIT
+        }
+    };
 
     SPI_Config();
     LCD_Config();
