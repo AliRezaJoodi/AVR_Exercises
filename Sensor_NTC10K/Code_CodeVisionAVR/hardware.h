2516ea0d55b2868d32bb6c1cd5909eefcef39328
Ali Reza Joodi <AliRezaJoodi@outlook.com>
2026-04-07 05:22:48 +0330

    chore: update project, , Sensor_NTC10K

---
+++ Sensor_NTC10K/Code_CodeVisionAVR/hardware.h
@@ -0,0 +1,11 @@
+#ifndef HARDWARE_INCLUDED
+#define HARDWARE_INCLUDED
+    #define ADC_VREF_MV             5000
+    #define ADC_RESOLUTION_BITS     10
+
+    #define NTC10K_RESISTOR         10000UL    //ohm
+    #define NTC10K_CH               0
+
+#endif
+
+
