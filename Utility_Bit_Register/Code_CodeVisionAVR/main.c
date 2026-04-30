9a4b4f0075091c9bfcd2f9d2d37ae28cd2ff0680
Ali Reza Joodi <AliRezaJoodi@outlook.com>
2026-04-30 20:31:17 +0330

    chore: update "Utility_Bit_Register"

--- Utility_Bit_Register/Code_CodeVisionAVR/main.c
+++ Utility_Bit_Register/Code_CodeVisionAVR/main.c
@@ -12,39 +12,63 @@ void main(void){
         volatile uint8_t *port;
         volatile uint8_t *pin;
         const uint8_t    index;
+        const uint8_t    mask;
     } Pin_t;
 
     Pin_t led = {
         .ddr   = &DDRA,
         .port  = &PORTA,
         .pin   = &PINA,
-        .index = 0
+        .index = 0U,
+        .mask  = (1U << 0U)
+    };
+
+    Pin_t led2 = {
+        .ddr   = &DDRA,
+        .port  = &PORTA,
+        .pin   = &PINA,
+        .index = 1U,
+        .mask  = (1U << 1U)
     };
 
     Pin_t key = {
         .ddr   = &DDRB,
         .port  = &PORTB,
         .pin   = &PINB,
-        .index = 3
+        .index = 3U,
+        .mask = (1U << 3U)
     };
 
 //    uint8_t value = 0;
 
     delay_ms(1000);
+    SetBit_Reg8(led.ddr, led.index); delay_ms(1000); // config output
 
-    SetBit_Reg8(led.ddr, led.index); delay_ms(500); // config output
     SetBit_Reg8(led.port, led.index); delay_ms(500);
     ClearBit_Reg8(led.port, led.index); delay_ms(1000);
+
     WriteBit_Reg8(led.port, led.index, 1); delay_ms(500);
     WriteBit_Reg8(led.port, led.index, 0); delay_ms(1000);
+
     ToggleBit_Reg8(led.port, led.index); delay_ms(500);
     ToggleBit_Reg8(led.port, led.index); delay_ms(1000);
 
-    ClearBit_Reg8(key.ddr, key.index); delay_ms(500);  // config input
-    SetBit_Reg8(key.port, key.index); delay_ms(500);   // config pull-up
+
+    SetBitMask_Reg8(led2.ddr, led2.mask); delay_ms(1000); // config output
+
+    SetBitMask_Reg8(led2.port, led2.mask); delay_ms(500);
+    ClearBitMask_Reg8(led2.port, led2.mask); delay_ms(1000);
+
+    ToggleBitMask_Reg8(led2.port, led2.mask); delay_ms(500);
+    ToggleBitMask_Reg8(led2.port, led2.mask); delay_ms(1000);
+
+
+    ClearBit_Reg8(key.ddr, key.index); delay_ms(10);  // config input
+    SetBit_Reg8(key.port, key.index); delay_ms(10);   // config pull-up
 
     while(1){
-         WriteBit_Reg8(led.port, led.index, GetBit_Reg8(key.pin, key.index));
+         WriteBit_Reg8(led.port, led.index, ReadBit_Reg8(key.pin, key.index));
+         WriteBit_Reg8(led2.port, led2.index, ReadBitMask_Reg8(key.pin, key.mask));
     };
 }
 
