5794316cba7cec354f8530fbd1b92b20918d9fbf
Ali Reza Joodi <AliRezaJoodi@outlook.com>
2026-05-24 17:37:34 +0330

    chore: update, Buzzer

--- Buzzer/Code_CodeVisionAVR/hardware.h
+++ Buzzer/Code_CodeVisionAVR/hardware.h
@@ -1,11 +1,19 @@
 #ifndef HARDWARE_INCLUDED
 #define HARDWARE_INCLUDED
 
+/*
+ * Choose ONLY ONE configuration below:
+ * - TIMEBASE_REG8_U8    (8-bit registers, 8-bit counter)
+ * - TIMEBASE_REG8_U32   (8-bit registers, 32-bit counter)
+ * - TIMEBASE_REG32_U32  (32-bit registers, 32-bit counter)
+ */
+#define TIMEBASE_CONFIG
+    #define TIMEBASE_REG8_U8
+
 #define BUZZER_HARDWARE
     #define BUZZER_ACTIVE       1U
-    #define BUZZER_COUNT1       10000U
-    #define BUZZER_COUNT2       25000U
-    #define BUZZER_COUNT3       60000U
+    #define BUZZER_COUNT1       75U
+    #define BUZZER_COUNT2       180U
 
     #define BUZZER_DDR          DDRB
     #define BUZZER_PORT         PORTB
--- Buzzer/Code_CodeVisionAVR/main.c
+++ Buzzer/Code_CodeVisionAVR/main.c
@@ -5,16 +5,61 @@
 #include <delay.h>
 
 #include "hardware.h"
+#include "timebase.h"
+#include "bit_register8.h"
 #include "buzzer.h"
 
+void Timer2_Init(void);
+
+// Timer2 overflow interrupt service routine
+interrupt [TIM2_OVF] void timer2_ovf_isr(void){
+    TCNT2=0x06; // Reinitialize Timer2 value
+
+    TimeBase_CountTicks();
+}
+
 void main(void){
+    uint32_t last_tick = 0;
+    uint32_t now_tick = 0;
+
     Buzzer_Init();
 
-    Buzzer_Start(BUZZER_COUNT2);
+    Timer2_Init();
+    // Timer(s)/Counter(s) Interrupt(s) initialization
+    TIMSK=(0<<OCIE2) | (1<<TOIE2) | (0<<TICIE1) | (0<<OCIE1A) | (0<<OCIE1B) | (0<<TOIE1) | (0<<OCIE0) | (0<<TOIE0);
+
+    #asm("sei") // Globally enable interrupts
+
+    DDRD.3 = 1;
+    PORTD.3 = 0;
+    last_tick = TimeBase_GetTicks();
+
+    Buzzer_Start(BUZZER_COUNT1);
 
     while(1){
+        now_tick = TimeBase_GetTicks();
+
+        if ((now_tick - last_tick) >= 250){
+            last_tick = now_tick;
+            ToggleBit_Reg8(&PORTD, 3);
+        }
+
         Buzzer_Refresh();
     }
+}
+
+//********************************************************
+void Timer2_Init(void){
+    // Timer/Counter 2 initialization
+    // Clock source: System Clock
+    // Clock value: 250.000 kHz
+    // Mode: Normal top=0xFF
+    // OC2 output: Disconnected
+    // Timer Period: 1 ms
+    ASSR=0<<AS2;
+    TCCR2=(0<<PWM2) | (0<<COM21) | (0<<COM20) | (0<<CTC2) | (0<<CS22) | (1<<CS21) | (1<<CS20);
+    TCNT2=0x06;
+    OCR2=0x00;
 }
 
 
