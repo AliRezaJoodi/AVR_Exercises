9e1b94d766319b240e4188d9e2a3be2e58a11fec
Ali Reza Joodi <AliRezaJoodi@outlook.com>
2026-05-30 11:00:51 +0330

    chore: update, GPIO

--- GPIO/Code_CodeVisionAVR/main.c
+++ GPIO/Code_CodeVisionAVR/main.c
@@ -12,12 +12,12 @@ void main(void){
 
     typedef struct{
         const GPIO_PortReg_t *gpio;
-        const GPIO_Pin_t index;
+        const GPIO_PinPos_t index;
     } LED_t;
 
     static const LED_t LED2 = {
         .gpio   = &GPIOB,
-        .index  = GPIO_PIN_1,
+        .index  = GPIO_PIN_1_POS,
     };
 
     //Init Pins for LED2
@@ -25,8 +25,8 @@ void main(void){
     GPIO_WritePin(LED2.gpio, LED2.index, 1);
 
     //Init Pins for LED
-    GPIO_ConfigDirection(&GPIOB, GPIO_PIN_0, GPIO_DIR_OUTPUT);
-    GPIO_WritePin(&GPIOB, GPIO_PIN_0, 0);
+    GPIO_ConfigDirection(&GPIOB, GPIO_PIN_0_POS, GPIO_DIR_OUTPUT);
+    GPIO_WritePin(&GPIOB, GPIO_PIN_0_POS, 0);
 //    GPIO_SetPin(&GPIOB, GPIO_PIN_0); delay_ms(1000);
 //    GPIO_ClearPin(&GPIOB, GPIO_PIN_0); delay_ms(1000);
 //    GPIO_SetPinMask(&GPIOB, GPIO_PINMASK_0); delay_ms(1000);
@@ -37,50 +37,50 @@ void main(void){
 //    GPIO_TogglePin(&GPIOB, GPIO_PIN_0); delay_ms(1000);
 
     //Init Pins for Button_On
-    GPIO_ConfigDirection(&GPIOA, GPIO_PIN_0, GPIO_DIR_INPUT);
-    GPIO_ConfigPull(&GPIOA, GPIO_PIN_0, GPIO_PULL_UP);
+    GPIO_ConfigDirection(&GPIOA, GPIO_PIN_0_POS, GPIO_DIR_INPUT);
+    GPIO_ConfigPull(&GPIOA, GPIO_PIN_0_POS, GPIO_PULL_UP);
 
     //Init Pins for Button_Off
-    GPIO_ConfigDirection(&GPIOA, GPIO_PIN_1, GPIO_DIR_INPUT);
-    GPIO_ConfigPull(&GPIOA, GPIO_PIN_1, GPIO_PULL_NONE);
+    GPIO_ConfigDirection(&GPIOA, GPIO_PIN_1_POS, GPIO_DIR_INPUT);
+    GPIO_ConfigPull(&GPIOA, GPIO_PIN_1_POS, GPIO_PULL_NONE);
 
     //Init Pins for 4-channel input
-    GPIO_ConfigDirection(&GPIOB, GPIO_PIN_3, GPIO_DIR_INPUT);
-    GPIO_ConfigPull(&GPIOA, GPIO_PIN_3, GPIO_PULL_NONE);
+    GPIO_ConfigDirection(&GPIOB, GPIO_PIN_3_POS, GPIO_DIR_INPUT);
+    GPIO_ConfigPull(&GPIOA, GPIO_PIN_3_POS, GPIO_PULL_NONE);
 
-    GPIO_ConfigDirection(&GPIOB, GPIO_PIN_4, GPIO_DIR_INPUT);
-    GPIO_ConfigPull(&GPIOA, GPIO_PIN_4, GPIO_PULL_NONE);
+    GPIO_ConfigDirection(&GPIOB, GPIO_PIN_4_POS, GPIO_DIR_INPUT);
+    GPIO_ConfigPull(&GPIOA, GPIO_PIN_4_POS, GPIO_PULL_NONE);
 
-    GPIO_ConfigDirection(&GPIOB, GPIO_PIN_5, GPIO_DIR_INPUT);
-    GPIO_ConfigPull(&GPIOA, GPIO_PIN_5, GPIO_PULL_NONE);
+    GPIO_ConfigDirection(&GPIOB, GPIO_PIN_5_POS, GPIO_DIR_INPUT);
+    GPIO_ConfigPull(&GPIOA, GPIO_PIN_5_POS, GPIO_PULL_NONE);
 
-    GPIO_ConfigDirection(&GPIOB, GPIO_PIN_6, GPIO_DIR_INPUT);
-    GPIO_ConfigPull(&GPIOA, GPIO_PIN_6, GPIO_PULL_NONE);
+    GPIO_ConfigDirection(&GPIOB, GPIO_PIN_6_POS, GPIO_DIR_INPUT);
+    GPIO_ConfigPull(&GPIOA, GPIO_PIN_6_POS, GPIO_PULL_NONE);
 
     //Init Pins for 4-pin 7-Segment
-    GPIO_ConfigDirection(&GPIOC, GPIO_PIN_2, GPIO_DIR_OUTPUT);
-    GPIO_WritePin(&GPIOC, GPIO_PIN_2, 0);
+    GPIO_ConfigDirection(&GPIOC, GPIO_PIN_2_POS, GPIO_DIR_OUTPUT);
+    GPIO_WritePin(&GPIOC, GPIO_PIN_2_POS, 0);
 
-    GPIO_ConfigDirection(&GPIOC, GPIO_PIN_3, GPIO_DIR_OUTPUT);
-    GPIO_WritePin(&GPIOC, GPIO_PIN_3, 0);
+    GPIO_ConfigDirection(&GPIOC, GPIO_PIN_3_POS, GPIO_DIR_OUTPUT);
+    GPIO_WritePin(&GPIOC, GPIO_PIN_3_POS, 0);
 
-    GPIO_ConfigDirection(&GPIOC, GPIO_PIN_4, GPIO_DIR_OUTPUT);
-    GPIO_WritePin(&GPIOC, GPIO_PIN_4, 0);
+    GPIO_ConfigDirection(&GPIOC, GPIO_PIN_4_POS, GPIO_DIR_OUTPUT);
+    GPIO_WritePin(&GPIOC, GPIO_PIN_4_POS, 0);
 
-    GPIO_ConfigDirection(&GPIOC, GPIO_PIN_5, GPIO_DIR_OUTPUT);
-    GPIO_WritePin(&GPIOC, GPIO_PIN_5, 0);
+    GPIO_ConfigDirection(&GPIOC, GPIO_PIN_5_POS, GPIO_DIR_OUTPUT);
+    GPIO_WritePin(&GPIOC, GPIO_PIN_5_POS, 0);
 
     while(1){
-        if(GPIO_ReadPin(&GPIOA, GPIO_PIN_0) == 0){
-			GPIO_SetPin(&GPIOB, GPIO_PIN_0);
+        if(GPIO_ReadPin(&GPIOA, GPIO_PIN_0_POS) == 0){
+			GPIO_SetPin(&GPIOB, GPIO_PIN_0_POS);
 		}
 
-		if(GPIO_ReadPin(&GPIOA, GPIO_PIN_1) == 0){
-			GPIO_ClearPin(&GPIOB, GPIO_PIN_0);
+		if(GPIO_ReadPin(&GPIOA, GPIO_PIN_1_POS) == 0){
+			GPIO_ClearPin(&GPIOB, GPIO_PIN_0_POS);
 		}
 
-	    value = GPIO_Read4Pin(&GPIOB, GPIO_PIN_3);
-		GPIO_Write4Pin(&GPIOC, GPIO_PIN_2, value);
+	    value = GPIO_Read4Pin(&GPIOB, GPIO_PIN_3_POS);
+		GPIO_Write4Pin(&GPIOC, GPIO_PIN_2_POS, value);
     }
 }
 
