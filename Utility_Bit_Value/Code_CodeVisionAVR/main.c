ce392cb00460a1a84f67fd1c23bba92cf44ef6bd
Ali Reza Joodi <AliRezaJoodi@outlook.com>
2026-06-02 23:49:52 +0330

    Utility_Bit_Value

---
+++ Utility_Bit_Value/Code_CodeVisionAVR/main.c
@@ -0,0 +1,56 @@
+// GitHub Account:  GitHub.com/AliRezaJoodi
+
+#include <stdint.h>
+#include <mega32a.h>
+
+#include "aj_bit_u8.h"
+#include "aj_bit_u16.h"
+#include "aj_bit_u32.h"
+
+void Test_u32(void);
+void Test_u16(void);
+void Test_u8(void);
+
+void main(void){
+    DDRA = 0xFFU;
+    DDRB = 0xFFU;
+    DDRC = 0xFFU;
+    DDRD = 0xFFU;
+
+    Test_u32();
+
+    while(1){
+    };
+}
+
+void Test_u32(void){
+    uint32_t value_u32 = 0x80000001UL;
+
+    value_u32 = AJ_BitU32_SetBits_Mask(value_u32, 0xF000030FUL);
+
+    while(1){
+        PORTA =  AJ_BitU32_Get8Bits_Position(value_u32, 0U);
+        PORTB =  AJ_BitU32_Get8Bits_Position(value_u32, 8U);
+        PORTC =  AJ_BitU32_Get8Bits_Position(value_u32, 16U);
+        PORTD =  AJ_BitU32_Get8Bits_Position(value_u32, 24U);
+    };
+}
+
+void Test_u16(void){
+    uint16_t value_u16 = 0x8001UL;
+
+    while(1){
+        PORTA =  AJ_BitU32_Get8Bits_Position(value_u16, 0U);
+        PORTB =  AJ_BitU32_Get8Bits_Position(value_u16, 8U);
+    };
+}
+
+void Test_u8(void){
+    uint8_t value_u8 = 0x81UL;
+
+    while(1){
+        PORTA =  AJ_BitU32_Get8Bits_Position(value_u8, 0U);
+    };
+}
+
+
---
+++ Utility_Bit_Value/README.md
@@ -0,0 +1,20 @@
+## utils/aj_bit_ux.h
+Test my library from CrossPlatformLibraries.  
+
+Include:
+- aj_bit_u8.h
+- aj_bit_u16.h
+- aj_bit_u32.h
+
+### Simulate: v1.0
+![](Simulate/v1.0.png)
+
+### Features
+- **MCU:** ATmega32
+ 
+### Folder and Files Description
+- `Code_CodeVisionAVR` (Code with C Language)
+- `Simulate` (Simulator File)
+
+My GitHub Account: [GitHub.com/AliRezaJoodi](https://github.com/AliRezaJoodi)  
+**Note**: [You can go here to download a single folder or file from GitHub.com](https://minhaskamal.github.io/DownGit/#/home)
