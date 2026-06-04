33f44fee267203f873f4b0640f35d6293598bff2
Ali Reza Joodi <AliRezaJoodi@outlook.com>
2026-06-04 12:38:17 +0330

    chore: update, GPIO

--- GPIO/Code_CodeVisionAVR/hardware.h
+++ GPIO/Code_CodeVisionAVR/hardware.h
@@ -3,12 +3,15 @@
 #ifndef HARDWARE_INCLUDED
 #define HARDWARE_INCLUDED
 
-#define GPIO_CONFIG
-    #define MCU_SUPPORT_PORTA    1U
-    #define MCU_SUPPORT_PORTB    1U
-    #define MCU_SUPPORT_PORTC    1U
-    #define MCU_SUPPORT_PORTD    0U
-    #define MCU_SUPPORT_PORTE    0U
-    #define MCU_SUPPORT_PORTF    0U
+#define AJ_BIT_REG_CONFIG
+    #define AJ_BIT_REG_8BIT
+
+#define AJ_GPIO_CONFIG
+    #define AJ_MCU_SUPPORT_PORTA    1U
+    #define AJ_MCU_SUPPORT_PORTB    1U
+    #define AJ_MCU_SUPPORT_PORTC    1U
+    #define AJ_MCU_SUPPORT_PORTD    0U
+    #define AJ_MCU_SUPPORT_PORTE    0U
+    #define AJ_MCU_SUPPORT_PORTF    0U
 
 #endif  /* HARDWARE_INCLUDED */
--- GPIO/Code_CodeVisionAVR/main.c
+++ GPIO/Code_CodeVisionAVR/main.c
@@ -5,82 +5,89 @@
 #include <delay.h>
 
 #include "hardware.h"
-#include "gpio.h"
+#include "aj_gpio.h"
 
 void main(void){
     uint8_t value = 0;
 
     typedef struct{
-        const GPIO_PortReg_t *gpio;
-        const GPIO_PinPos_t index;
+        const AJ_GPIO_PortReg_t *gpio;
+        const AJ_GPIO_PinPos_t index;
     } LED_t;
 
     static const LED_t LED2 = {
         .gpio   = &GPIOB,
-        .index  = GPIO_PIN_1_POS,
+        .index  = AJ_GPIO_PIN_1_POS,
     };
 
+//    AJ_GPIO_ConfigPinsAsInput_Mask(&GPIOC, AJ_GPIO_PIN_0_MASK | AJ_GPIO_PIN_1_MASK | AJ_GPIO_PIN_7_MASK);
+//    AJ_GPIO_EnablePullUp_Mask(&GPIOC, AJ_GPIO_PIN_0_MASK | AJ_GPIO_PIN_1_MASK | AJ_GPIO_PIN_7_MASK);
+//    AJ_GPIO_DisablePullUp_Mask(&GPIOC, AJ_GPIO_PIN_0_MASK | AJ_GPIO_PIN_1_MASK);
+//    AJ_GPIO_EnablePullUp_Position(&GPIOC, AJ_GPIO_PIN_0_POS);
+//    AJ_GPIO_DisablePullUp_Position(&GPIOC, AJ_GPIO_PIN_0_POS);
+//    while(1){}
+
     //Init Pins for LED2
-    GPIO_ConfigDirection(LED2.gpio, LED2.index, GPIO_DIR_OUTPUT);
-    GPIO_WritePin(LED2.gpio, LED2.index, 1);
+    AJ_GPIO_ConfigDirection(LED2.gpio, LED2.index, AJ_GPIO_DIR_OUTPUT);
+    AJ_GPIO_WritePin_Position(LED2.gpio, LED2.index, 1);
 
     //Init Pins for LED
-    GPIO_ConfigDirection(&GPIOB, GPIO_PIN_0_POS, GPIO_DIR_OUTPUT);
-    GPIO_WritePin(&GPIOB, GPIO_PIN_0_POS, 0);
-//    GPIO_SetPin(&GPIOB, GPIO_PIN_0); delay_ms(1000);
-//    GPIO_ClearPin(&GPIOB, GPIO_PIN_0); delay_ms(1000);
-//    GPIO_SetPinMask(&GPIOB, GPIO_PINMASK_0); delay_ms(1000);
-//    GPIO_ClearPinMask(&GPIOB, GPIO_PINMASK_0); delay_ms(1000);
-//    GPIO_TogglePinMask(&GPIOB, GPIO_PINMASK_0); delay_ms(1000);
-//    GPIO_TogglePinMask(&GPIOB, GPIO_PINMASK_0); delay_ms(1000);
-//    GPIO_TogglePin(&GPIOB, GPIO_PIN_0); delay_ms(1000);
-//    GPIO_TogglePin(&GPIOB, GPIO_PIN_0); delay_ms(1000);
+    AJ_GPIO_ConfigDirection(&GPIOB, AJ_GPIO_PIN_0_POS, AJ_GPIO_DIR_OUTPUT);
+    AJ_GPIO_WritePin_Position(&GPIOB, AJ_GPIO_PIN_0_POS, 0);
+//    AJ_GPIO_SetPin_Position(&GPIOB, AJ_GPIO_PIN_0); delay_ms(1000);
+//    AJ_GPIO_ClearPin_Position(&GPIOB, AJ_GPIO_PIN_0); delay_ms(1000);
+//    AJ_AJ_GPIO_SetPin_Positions_Mask(&GPIOB, AJ_GPIO_PINMASK_0); delay_ms(1000);
+//    AJ_GPIO_ClearPins_Mask(&GPIOB, AJ_GPIO_PINMASK_0); delay_ms(1000);
+//    AJ_GPIO_TogglePins_Mask(&GPIOB, AJ_GPIO_PINMASK_0); delay_ms(1000);
+//    AJ_GPIO_TogglePins_Mask(&GPIOB, AJ_GPIO_PINMASK_0); delay_ms(1000);
+//    AJ_GPIO_TogglePin_Position(&GPIOB, AJ_GPIO_PIN_0); delay_ms(1000);
+//    AJ_GPIO_TogglePin_Position(&GPIOB, AJ_GPIO_PIN_0); delay_ms(1000);
 
     //Init Pins for Button_On
-    GPIO_ConfigDirection(&GPIOA, GPIO_PIN_0_POS, GPIO_DIR_INPUT);
-    GPIO_ConfigPull(&GPIOA, GPIO_PIN_0_POS, GPIO_PULL_UP);
+    AJ_GPIO_ConfigDirection(&GPIOA, AJ_GPIO_PIN_0_POS, AJ_GPIO_DIR_INPUT);
+    AJ_GPIO_ConfigPull(&GPIOA, AJ_GPIO_PIN_0_POS, AJ_GPIO_PULL_UP);
 
     //Init Pins for Button_Off
-    GPIO_ConfigDirection(&GPIOA, GPIO_PIN_1_POS, GPIO_DIR_INPUT);
-    GPIO_ConfigPull(&GPIOA, GPIO_PIN_1_POS, GPIO_PULL_NONE);
+    AJ_GPIO_ConfigDirection(&GPIOA, AJ_GPIO_PIN_1_POS, AJ_GPIO_DIR_INPUT);
+    AJ_GPIO_ConfigPull(&GPIOA, AJ_GPIO_PIN_1_POS, AJ_GPIO_PULL_NONE);
 
     //Init Pins for 4-channel input
-    GPIO_ConfigDirection(&GPIOB, GPIO_PIN_3_POS, GPIO_DIR_INPUT);
-    GPIO_ConfigPull(&GPIOA, GPIO_PIN_3_POS, GPIO_PULL_NONE);
+    AJ_GPIO_ConfigDirection(&GPIOB, AJ_GPIO_PIN_3_POS, AJ_GPIO_DIR_INPUT);
+    AJ_GPIO_ConfigPull(&GPIOA, AJ_GPIO_PIN_3_POS, AJ_GPIO_PULL_NONE);
 
-    GPIO_ConfigDirection(&GPIOB, GPIO_PIN_4_POS, GPIO_DIR_INPUT);
-    GPIO_ConfigPull(&GPIOA, GPIO_PIN_4_POS, GPIO_PULL_NONE);
+    AJ_GPIO_ConfigDirection(&GPIOB, AJ_GPIO_PIN_4_POS, AJ_GPIO_DIR_INPUT);
+    AJ_GPIO_ConfigPull(&GPIOA, AJ_GPIO_PIN_4_POS, AJ_GPIO_PULL_NONE);
 
-    GPIO_ConfigDirection(&GPIOB, GPIO_PIN_5_POS, GPIO_DIR_INPUT);
-    GPIO_ConfigPull(&GPIOA, GPIO_PIN_5_POS, GPIO_PULL_NONE);
+    AJ_GPIO_ConfigDirection(&GPIOB, AJ_GPIO_PIN_5_POS, AJ_GPIO_DIR_INPUT);
+    AJ_GPIO_ConfigPull(&GPIOA, AJ_GPIO_PIN_5_POS, AJ_GPIO_PULL_NONE);
 
-    GPIO_ConfigDirection(&GPIOB, GPIO_PIN_6_POS, GPIO_DIR_INPUT);
-    GPIO_ConfigPull(&GPIOA, GPIO_PIN_6_POS, GPIO_PULL_NONE);
+    AJ_GPIO_ConfigDirection(&GPIOB, AJ_GPIO_PIN_6_POS, AJ_GPIO_DIR_INPUT);
+    AJ_GPIO_ConfigPull(&GPIOA, AJ_GPIO_PIN_6_POS, AJ_GPIO_PULL_NONE);
 
     //Init Pins for 4-pin 7-Segment
-    GPIO_ConfigDirection(&GPIOC, GPIO_PIN_2_POS, GPIO_DIR_OUTPUT);
-    GPIO_WritePin(&GPIOC, GPIO_PIN_2_POS, 0);
+    AJ_GPIO_ConfigDirection(&GPIOC, AJ_GPIO_PIN_2_POS, AJ_GPIO_DIR_OUTPUT);
+    AJ_GPIO_WritePin_Position(&GPIOC, AJ_GPIO_PIN_2_POS, 0);
 
-    GPIO_ConfigDirection(&GPIOC, GPIO_PIN_3_POS, GPIO_DIR_OUTPUT);
-    GPIO_WritePin(&GPIOC, GPIO_PIN_3_POS, 0);
+    AJ_GPIO_ConfigDirection(&GPIOC, AJ_GPIO_PIN_3_POS, AJ_GPIO_DIR_OUTPUT);
+    AJ_GPIO_WritePin_Position(&GPIOC, AJ_GPIO_PIN_3_POS, 0);
 
-    GPIO_ConfigDirection(&GPIOC, GPIO_PIN_4_POS, GPIO_DIR_OUTPUT);
-    GPIO_WritePin(&GPIOC, GPIO_PIN_4_POS, 0);
+    AJ_GPIO_ConfigDirection(&GPIOC, AJ_GPIO_PIN_4_POS, AJ_GPIO_DIR_OUTPUT);
+    AJ_GPIO_WritePin_Position(&GPIOC, AJ_GPIO_PIN_4_POS, 0);
 
-    GPIO_ConfigDirection(&GPIOC, GPIO_PIN_5_POS, GPIO_DIR_OUTPUT);
-    GPIO_WritePin(&GPIOC, GPIO_PIN_5_POS, 0);
+    AJ_GPIO_ConfigDirection(&GPIOC, AJ_GPIO_PIN_5_POS, AJ_GPIO_DIR_OUTPUT);
+    AJ_GPIO_WritePin_Position(&GPIOC, AJ_GPIO_PIN_5_POS, 0);
 
     while(1){
-        if(GPIO_ReadPin(&GPIOA, GPIO_PIN_0_POS) == 0){
-			GPIO_SetPin(&GPIOB, GPIO_PIN_0_POS);
+        if(AJ_GPIO_ReadPin_Position(&GPIOA, AJ_GPIO_PIN_0_POS) == 0){
+			AJ_GPIO_SetPin_Position(&GPIOB, AJ_GPIO_PIN_0_POS);
 		}
 
-		if(GPIO_ReadPin(&GPIOA, GPIO_PIN_1_POS) == 0){
-			GPIO_ClearPin(&GPIOB, GPIO_PIN_0_POS);
+		if(AJ_GPIO_ReadPin_Position(&GPIOA, AJ_GPIO_PIN_1_POS) == 0){
+			AJ_GPIO_ClearPin_Position(&GPIOB, AJ_GPIO_PIN_0_POS);
 		}
 
-	    value = GPIO_Read4Pin(&GPIOB, GPIO_PIN_3_POS);
-		GPIO_Write4Pin(&GPIOC, GPIO_PIN_2_POS, value);
+	    value = AJ_GPIO_Read4Pins_Position(&GPIOB, AJ_GPIO_PIN_3_POS);
+		AJ_GPIO_Write4Pins_Position(&GPIOC, AJ_GPIO_PIN_2_POS, value);
     }
 }
 
