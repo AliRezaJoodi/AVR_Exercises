fb47169aac9c5bd6d43e10ff390220601cb14ae9
Ali Reza Joodi <AliRezaJoodi@outlook.com>
2026-07-02 16:14:02 +0330

    chore: update, Dimmer_1Phase

--- Utility_Bit_Value/Code_CodeVisionAVR/main.c
+++ Utility_Bit_Value/Code_CodeVisionAVR/main.c
@@ -12,49 +12,52 @@ void Test_u16(void);
 void Test_u8(void);
 
 void main(void){
-    DDRA = 0xFFU;
-    DDRB = 0xFFU;
-    DDRC = 0xFFU;
-    DDRD = 0xFFU;
+	DDRA = 0xFFU;
+	DDRB = 0xFFU;
+	DDRC = 0xFFU;
+	DDRD = 0xFFU;
 
-    Test_u16();
+	Test_u16();
 
-    while(1){
-    };
+	while(1);
 }
 
 //******************************************************
-void Test_u32(void){
-    uint32_t value_u32 = 0x80000001UL;
+void Test_u32(void)
+{
+	uint32_t value_u32 = 0x80000001UL;
 
-    value_u32 = AJ_BitU32_SetBit_Mask(value_u32, 0x03000030UL);
+	value_u32 = AJ_BitU32_SetBit_Mask(value_u32, 0x03000030UL);
 
-    while(1){
-        //PORTA =  AJ_BitU32_Get8Bit_Position(value_u32, 0U);
-        PORTA =  AJ_BitU32_GetField_Position(value_u32, 0U, 8U);
-        PORTB =  AJ_BitU32_Get8Bit_Position(value_u32, 8U);
-        PORTC =  AJ_BitU32_Get8Bit_Position(value_u32, 16U);
-        PORTD =  AJ_BitU32_Get8Bit_Position(value_u32, 24U);
-    };
+	while(1)
+		{
+		//PORTA =  AJ_BitU32_Get8Bit_Position(value_u32, 0U);
+		PORTA =  AJ_BitU32_GetField_Position(value_u32, 0U, 8U);
+		PORTB =  AJ_BitU32_Get8Bit_Position(value_u32, 8U);
+		PORTC =  AJ_BitU32_Get8Bit_Position(value_u32, 16U);
+		PORTD =  AJ_BitU32_Get8Bit_Position(value_u32, 24U);
+		};
 }
 
 //******************************************************
-void Test_u16(void){
-    uint16_t value_u16 = 0x8001U;
+void Test_u16(void)
+{
+	uint16_t value_u16 = 0x8001U;
 
-    while(1){
-        PORTA =  AJ_BitU16_GetField_Position(value_u16, 0U, 8U);
-        PORTB =  AJ_BitU16_GetField_Position(value_u16, 8U, 8U);
-    };
+	while(1)
+		{
+		PORTA =  AJ_BitU16_GetField_Position(value_u16, 0U, 8U);
+		PORTB =  AJ_BitU16_GetField_Position(value_u16, 8U, 8U);
+		};
 }
 
 //******************************************************
-void Test_u8(void){
-    uint8_t value_u8 = 0b10110111;
+void Test_u8(void)
+{
+	uint8_t value_u8 = 0b10110111;
 
-    while(1){
-        PORTA =  AJ_BitU8_GetField_Position(value_u8, 1U, 7U);
-    };
+	while(1)
+		PORTA =  AJ_BitU8_GetField_Position(value_u8, 1U, 7U);;
 }
 
 
