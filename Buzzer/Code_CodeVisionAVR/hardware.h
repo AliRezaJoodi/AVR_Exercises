fd227af64bca8e697e8043045095ae64503624b2
Ali Reza Joodi <AliRezaJoodi@outlook.com>
2026-07-10 21:04:12 +0330

    chore: update

--- Buzzer/Code_CodeVisionAVR/hardware.h
+++ Buzzer/Code_CodeVisionAVR/hardware.h
@@ -1,18 +1,21 @@
 #ifndef HARDWARE_INCLUDED
 #define HARDWARE_INCLUDED
 
+
 #define AJ_TARGET_CONFIG
     #define AJ_TARGET_MCU_BITS     8U      /*!< Supported: 8, 16, 32 */
     #define AJ_TARGET_TICK_BITS    8U      /*!< Supported: 8, 16, 32 */
 
-#define AJ_BUZZER_HARDWARE
+#define AJ_BUZZER_CONFIG
     #define AJ_BUZZER_ACTIVE       1U
     #define AJ_BUZZER_COUNT1       75U
-    #define AJ_BUZZER_COUNT2       180U
+    #define AJ_BUZZER_COUNT2       200U
 
+#define AJ_BUZZER_CONFIG_PLATFORM
     #define AJ_BUZZER_DDR          DDRB
     #define AJ_BUZZER_PORT         PORTB
     #define AJ_BUZZER_POS          1U
     #define AJ_BUZZER_MASK         (1U << AJ_BUZZER_POS)
+
 
 #endif  /* HARDWARE_INCLUDED */
